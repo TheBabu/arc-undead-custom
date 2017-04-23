71b91bd2ad0042f1ae7a1724aaad08176e38ccf3
Christian Dürr <contact@christianduerr.com>
2017-04-23 17:11:34 +0200

    Add gtk2 theme again
    The gtk2 version of the theme has been added again. Also the corners in
    the assets.svg are not rounded anymore.

--- install.sh
+++ install.sh
@@ -13,16 +13,21 @@ rm -rf "$TARGET"
 
 # Create required directories
 mkdir -p "$TARGET/gtk-3.0"
+mkdir -p "$TARGET/gtk-2.0"
 
 # Move theme info to the root theme dir
 cp "./index.theme" "$TARGET/index.theme"
 
-# Copy main assets to the gtk folder
-cp -r "./assets" "$TARGET/gtk-3.0/assets"
+# Copy assets to the gtk3 folder
+cp -r "./gtk3/assets" "$TARGET/gtk-3.0/assets"
 
-# Copy css file to the gtk folder
-cp "./gtk.css" "$TARGET/gtk-3.0/gtk.css"
+# Copy css file to the gtk3 folder
+cp "./gtk3/gtk.css" "$TARGET/gtk-3.0/gtk.css"
 
-# Create empty gtk2 theme
-mkdir "$TARGET/gtk-2.0"
-touch "$TARGET/gtk-2.0/gtkrc"
+# Copy assets to gtk2 folder
+cp -r "./gtk2/assets" "$TARGET/gtk-2.0/assets"
+cp -r "./gtk2/menubar-toolbar" "$TARGET/gtk-2.0/menu-toolbar"
+
+# Copy rc files to the gtk2 folder
+cp "./gtk2/gtkrc" "$TARGET/gtk-2.0/"
+cp "./gtk2/main.rc" "$TARGET/gtk-2.0/"
